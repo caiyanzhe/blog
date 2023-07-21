aba1602ad28ccca8132a4269e524dd9768ef5617
caiyanzhe <73111386+caiyanzhe@users.noreply.github.com>
2023-07-21 22:08:34 +0800

    Create bc.php

---
+++ bc.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<!-- saved from url=(0018)https://n-bc.com/ -->
+<html mip="" class="mip-i-android-scroll"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		
+		<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+	   	<link rel="canonical" href="https://n-bc.com/">
+		<title>新白菜频道 - 白菜同学的避风港</title>
+		<meta name="description" content="新白菜频道创立于2020年5月，主要分享一些日常生活与对算法编程题的理解。">
+		
+		<style mip-custom="">
+			body { margin:auto; padding:1.2em; font-size:1.2em; line-height:1.6; max-width:660px; }
+			h1 {font-size:1.6em; margin-bottom: 1.2em;}
+			h2 {font-size:1.4em; margin-bottom: 1.2em;}
+			ol{padding-left: 1.2em; margin-bottom: 1.2em;}
+			p{margin-bottom: 1.2em;}
+			li{margin-bottom: 0.6em;}
+			.red { color: #0099FF;}
+			a, .green { color: #0099FF;}
+		</style>
+	</head>
+	<body mip-ready="" class="mip-i-android-scroll"><mip-fixed type="top" class="mip-shell-header-wrapper mip-element" mipdata-fixedidx="Fixed0" style="display: none; z-index: 10000;"><div class="mip-shell-header transition" style="background-color: rgb(255, 255, 255);"><a href="javascript:void(0)" class="back-button" mip-header-btn="" data-button-name="back"><svg t="1530857979993" class="icon" style="fill: rgb(0, 0, 0);" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3173" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="currentColor" d="M348.949333 511.829333L774.250667 105.728C783.978667 96 789.333333 83.712 789.333333 71.104c0-12.629333-5.354667-24.917333-15.082666-34.645333-9.728-9.728-22.037333-15.082667-34.645334-15.082667-12.586667 0-24.917333 5.333333-34.624 15.082667L249.557333 471.616A62.570667 62.570667 0 0 0 234.666667 512c0 10.410667 1.130667 25.408 14.890666 40.042667l455.424 435.605333c9.706667 9.728 22.016 15.082667 34.624 15.082667s24.917333-5.354667 34.645334-15.082667c9.728-9.728 15.082667-22.037333 15.082666-34.645333 0-12.608-5.354667-24.917333-15.082666-34.645334L348.949333 511.829333z" p-id="3174"></path></svg></a><div class="mip-shell-header-logo-title"><span class="mip-shell-header-title" style="color: rgb(0, 0, 0);">WordPress JAM - WordPress 建站专家</span></div></div></mip-fixed>
+		<h1>新白菜频道 - 白菜同学的避风港</h1>
+
+		
+		<p>搞那些花哨的主页，莫啥用啊啊啊啊 :-) </p>
+
+		<p>我的WordPress 博客（速度还彳亍：<a data-type="mip" data-title="新白菜频道" href="http://blog.n-bc.top/">新白菜频道</a>。</p>
+
+		<h2>白菜是谁？</h2>
+
+		<p>Name:Cabbage</p>
+
+		<ol>
+		<li>Gender:Male</li>
+		<li><span class="green">Age:After 2010</span></li>
+		<li>Graduated From:<span class="red">(Want)</span>Hangzhou Middle School;Hangzhou Xuejun Middle School；Tsinghua University</li>
+		<li>2020.05网站搭建在Wordpress.com上。</li>
+		<li>2021.03更换到“凡科建站”（看上去很不专业哈，当时也不懂）</li>
+		<li>2021.08更换到星辰云2元/月的虚拟主机上，7块买了一个域名：<span class="red">caiyanzhe.xyz</span>。</li>
+		<li>2022-2023更换<span class="red">cbwt.asia,cbwt.site</span>。</li>
+		</ol>
+
+		<p>2023.06 更换<span class="red">n-bc.top。</span></p>
+		<p>注：如果你想和我聊天，欢迎加微信号：CBWT2010或发邮箱：<a href="mailto:cbwt123@yeah.net">cbwt123@yeah.net</a></p>
+
+		<p><a href="https://icp.gov.moe/">萌ICP备20232040号</a></p>
+
+		<mip-stats-baidu token="0GTvHca9y5zn3gAD" class="mip-element mip-layout-container"><script src="./hm.js"></script></mip-stats-baidu>
+
+		<script src="./mip.js"></script><mip-shell class="mip-element mip-layout-container"><script type="application/json">{"ignoreWarning": true}</script></mip-shell>
+		<script src="./mip-stats-baidu.js"></script>
+	
+<mip-fixed class="mip-shell-more-button-mask mip-element" mipdata-fixedidx="Fixed1" style="z-index: 9999; display: none;"></mip-fixed><mip-fixed class="mip-shell-more-button-wrapper mip-element" mipdata-fixedidx="Fixed2" style="height: 0px; z-index: 9998; display: none;"></mip-fixed><mip-fixed class="mip-shell-header-mask mip-element" mipdata-fixedidx="Fixed3" style="z-index: 9997; display: none;"></mip-fixed><mip-fixed id="mip-page-loading-wrapper" class="mip-page-loading-wrapper mip-element" mipdata-fixedidx="Fixed4" style="z-index: 9996; display: none;"><div class="mip-shell-header"><span mip-header-btn="" class="back-button"><svg t="1530857979993" class="icon" style="" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3173" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="currentColor" d="M348.949333 511.829333L774.250667 105.728C783.978667 96 789.333333 83.712 789.333333 71.104c0-12.629333-5.354667-24.917333-15.082666-34.645333-9.728-9.728-22.037333-15.082667-34.645334-15.082667-12.586667 0-24.917333 5.333333-34.624 15.082667L249.557333 471.616A62.570667 62.570667 0 0 0 234.666667 512c0 10.410667 1.130667 25.408 14.890666 40.042667l455.424 435.605333c9.706667 9.728 22.016 15.082667 34.624 15.082667s24.917333-5.354667 34.645334-15.082667c9.728-9.728 15.082667-22.037333 15.082666-34.645333 0-12.608-5.354667-24.917333-15.082666-34.645334L348.949333 511.829333z" p-id="3174"></path></svg></span><div class="mip-shell-header-logo-title"><img class="mip-shell-header-logo" src="https://wpjam.com/"><span class="mip-shell-header-title"></span></div></div></mip-fixed></body></html>
